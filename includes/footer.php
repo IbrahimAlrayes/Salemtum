0c8b1edc18906daacf3c4c94f7aa527ccaa69b42
IbrahimAlrayes <96233417+IbrahimAlrayes@users.noreply.github.com>
2022-08-19 15:47:23 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,79 @@
+<!-- <div class="site-section bg-secondary bg-image" style="background-image: url('images/FDA.jpg');"> -->
+      <div class="container">
+        <div class="row align-items-stretch">
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <a href="store.php" class="banner-1 h-100 d-flex" style="background-image: url('images/hal.jpg');">
+              <div class="banner-1-inner align-self-center">
+                <h2>Products</h2>
+                <p>At Salemtum, we ensure that you get high-quality life-saving medicines are delivered to you on time. 
+                </p>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <a href="index.php" class="banner-1 h-100 d-flex" style="background-image: url('images/Salemtum3.jpg');">
+              <div class="banner-1-inner ml-auto  align-self-center">
+                <h2>We Care</h2>
+                <p>Salemtum will focus on boosting public health and disease prevention, in addition to improving access to health services</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    <!-- </div> -->
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
+
+                <div class="block-7">
+                    <h3 class="footer-heading mb-4">About Us</h3>
+                    <p>All copy rights perserved to Salemtum company © 2022  </p>
+                </div>
+
+            </div>
+            <div class="col-lg-3 mx-auto mb-5 mb-lg-0">
+                <!-- <h3 class="footer-heading mb-4">Quick Links</h3>
+                <ul class="list-unstyled">
+                    <li><a href="#">Supplements</a></li>
+                    <li><a href="#">Vitamins</a></li>
+                    <li><a href="#">Diet &amp; Nutrition</a></li>
+                    <li><a href="#">Tea &amp; Coffee</a></li>
+                </ul> -->
+            </div>
+
+            <div class="col-md-6 col-lg-3">
+                <div class="block-5 mb-5">
+                    <h3 class="footer-heading mb-4">Contact Info</h3>
+                    <ul class="list-unstyled">
+                        <li class="address">King Saud University</li>
+                        <li class="phone"><a href="">+966 55 676 999</a></li>
+                        <li class="email">SWE481@gmail.com</li>
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
+        <!-- <div class="row pt-5 mt-5 text-center">
+            <div class="col-md-12">
+                <p>
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script> All rights reserved | This website was made
+                    with by <a href="https://md-taha-ahmed.github.io/Portfolio-website/" target="_blank" class="text-primary">MD TAHA AHMED</a>
+                </p>
+            </div>
+
+        </div> -->
+    </div>
+</footer>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/main.js"></script>
